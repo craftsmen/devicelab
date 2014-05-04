f72fee1eef655a04350cd210594eec968c5e3416
Mehdi Lahmam <mehdi@craftsmen.io>
2014-05-04 06:34:08 +0200

    Add necessary files and config for Heroku deploys

--- Gemfile
+++ Gemfile
@@ -1,4 +1,9 @@
 source 'https://rubygems.org'
 
+ruby '2.1.1'
+
 gem 'middleman'
 gem 'middleman-livereload'
+gem 'puma'
+gem 'rack-contrib'
+gem 'rake'
--- config.rb
+++ config.rb
@@ -44,18 +44,7 @@ ignore 'bower_components/*'
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+  activate :minify_css
+  activate :minify_javascript
+  activate :asset_hash
 end
